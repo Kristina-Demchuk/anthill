3215cf3de4e2f1b22a70c03e8358193efc2d018d
Kristina-Demchuk <kristika2012@mail.ru>
2014-01-31 22:48:33 +0300

    File main.java was renamed into HelloDate.java. It was corrected all mistakes.

---
+++ HelloDate.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+public class HelloDate {
+	public static void main(String[] args) {
+		System.out.print("Hello, today is ");
+		System.out.print(new Date());
+	}
+}
--- main.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-import static net.mindview.util.print.*;
-
-public class HelloDate {
-	public static void main(String[] args) {
-		print("Привет, сегодня");
-		print(new Date());
-	}
-}
